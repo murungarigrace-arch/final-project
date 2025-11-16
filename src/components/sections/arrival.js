69761b4fe52241054b4a2e16aca60a7a00924945
mrolystone-art <mrolystone@gmail.com>
2025-11-16 07:50:26 +0000

    iiii

--- src/components/sections/arrival.js
+++ src/components/sections/arrival.js
@@ -1,17 +1,17 @@
 export const newProducts=[
     {
         id:1,
-        name:"Blue Denim rugged Jeans",
+        name:"ford gmc",
         imgUrl:'https://i.pinimg.com/736x/15/81/a0/1581a084cfaae0b8b9cb16597f3957e2.jpg' //string
     },
      {
         id:2,
-        name:"ford gmc",
+        name:"landcrusier txl",
         imgUrl:'https://i.pinimg.com/736x/9b/f1/53/9bf1537bc6e5564199e1f8fe02fbb502.jpg' //string
     },
      {
         id:3,
-        name:"landcrusier tx",
+        name:"lamboghini",
         imgUrl:'https://i.pinimg.com/736x/93/e9/37/93e9379eb0194aba745b6b8dfa821703.jpg' //string
     },
        {
--- src/components/sections/best.jsx
+++ src/components/sections/best.jsx
@@ -5,7 +5,7 @@ export const Best = () => {
     <div className='flex flex-col justify-center items-center space-y-6 p-10 bg-(--primary-color) text-(--text-color) lg:flex-row lg:space-x-10 lg:space-y-0'>
         <div className='space-y-4 max-w-lg text-center lg:text-left'>
             <h3 className='font-bold capitalize text-2xl'>
-                The best car plug around you😉
+                The best car distibutor around you
             </h3>
             <p className='font-medium capitalize text-md '>
                 We offer a wide range of high-quality car's that are perfect for any occasion. Whether you're looking for classic styles or the latest trends, we've got you covered. Our car's are made from durable materials and designed to provide a comfortable fit, ensuring you look and feel your best. Visit us today to find your perfect dream car!
